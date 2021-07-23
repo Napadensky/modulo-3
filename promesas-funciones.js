770e8cb061e880c5310525bd14ca20007c090a05
David Napadensky <davidnapadensky@gmail.com>
2021-07-23 18:16:38 -0300

    tarea-3

---
+++ promesas-funciones.js
@@ -0,0 +1,16 @@
+const contratarJugador = (contratado = false) => {
+  return new Promise((resolve, reject) => {
+    if (contratado) resolve('El jugador se incorpora al equipo')
+    else reject('El jugador y el equipo no llegaron a un acuerdo')
+  });
+};
+const thenMensaje = texto => console.log("Mensaje:", texto);
+const catchMensaje = texto => console.log(texto);
+
+contratarJugador()
+  .then(thenMensaje)
+  .catch(catchMensaje);
+
+contratarJugador(true)
+  .then(thenMensaje)
+  .catch(catchMensaje);
